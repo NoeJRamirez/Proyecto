20ce003f0a0b84b4f2ef4f1c511424fda26db44c
NJR7 <2203066382@cua.uam.mx>
2024-05-14 12:19:29 -0600

    agregar mostrar texto

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 
 from digital_clock import DigitalClock
 from utime import sleep
+from pantalla import Pantalla
 
-def clock_test()->None:
+def clock_test():
+    pantalla = Pantalla()
     clock=DigitalClock(23,59,50)
     while True:
         h,m,s=clock.get_time()
-        time=f'{h:02} : {m:02} : {s:02}'
-        print(time)
+        time_str=f'{h:02} : {m:02} : {s:02}'
+        pantalla.mostrar_texto(time_str)
+        #print(time)
         sleep(1)
         clock.increment()
+if __name__ == "__main__":
+    # Ejecutar las pruebas del reloj
+    clock_test()
